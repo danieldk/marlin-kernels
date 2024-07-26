c3eb1596ea2501e8f9ac9bfc2418cdde685c2dd4
Daniël de Kok <me@danieldk.eu>
2024-07-26 12:33:15 +0000

    Always compile for capability 8.0 and 9.0

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
+import os
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-extra_compile_args = []
+
+def append_nvcc_threads(nvcc_extra_args):
+    nvcc_threads = os.getenv("NVCC_THREADS") or "4"
+    return nvcc_extra_args + ["--threads", nvcc_threads]
+
+
+cc_flag = []
+cc_flag.append("-gencode")
+cc_flag.append("arch=compute_80,code=sm_80")
+cc_flag.append("-gencode")
+cc_flag.append("arch=compute_90,code=sm_90")
+
+extra_compile_args = {
+    "nvcc": append_nvcc_threads(
+        [
+            "-O3",
+            "-std=c++17",
+            "-U__CUDA_NO_HALF_OPERATORS__",
+            "-U__CUDA_NO_HALF_CONVERSIONS__",
+            "-U__CUDA_NO_HALF2_OPERATORS__",
+            "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+            "--expt-relaxed-constexpr",
+            "--expt-extended-lambda",
+            "--use_fast_math",
+        ]
+        + cc_flag
+    )
+}
 
 setup(
     name="marlin_kernels",
