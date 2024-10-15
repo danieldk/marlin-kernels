34e2438b16ab92e06903ae6c98c313edc1b1c52a
Daniël de Kok <me@danieldk.eu>
2024-10-15 09:35:26 +0200

    Add Machete kernels from vLLM (#4)
    Since the build generates files and requires different build flags
    for the Machete kernels, it makes more sense to vendor the vLLM
    build infrastructure than something homegrown.

--- marlin_kernels/__init__.py
+++ marlin_kernels/__init__.py
@@ -1,7 +1,10 @@
+from typing import List, Optional
 import torch
 
 # Insure that the ops are registered.
-import marlin_kernels._ops
+import marlin_kernels._marlin_kernels
+
+from ._core_ext import ScalarType
 from .scalar_type import scalar_types
 
 
@@ -17,9 +20,41 @@ def awq_marlin_repack(
     b_q_weight: torch.Tensor, size_k: int, size_n: int, num_bits: int
 ) -> torch.Tensor:
     """Repack AWQ parameters for GPTQ-Marlin."""
-    return torch.ops._marlin_kernels_ops.awq_marlin_repack(
+    return torch.ops._marlin_kernels.awq_marlin_repack(
         b_q_weight, size_k, size_n, num_bits
     )
+
+
+def machete_supported_schedules(btype: ScalarType) -> List[str]:
+    """Get the supported Machete schedules for the given scalar type."""
+    return torch.ops._marlin_kernels.machete_supported_schedules(btype)
+
+
+def machete_gemm(
+    A: torch.Tensor,
+    B: torch.Tensor,
+    num_bits: int,
+    scales: torch.Tensor,
+    zeros: Optional[torch.Tensor] = None,
+    group_size: Optional[int] = None,
+    C: Optional[torch.Tensor] = None,
+    alpha: Optional[float] = None,
+    beta: Optional[float] = None,
+    schedule: Optional[str] = None,
+) -> torch.Tensor:
+    """
+    Matrix multiplication using Machette kernels (requires Hopper or later).
+    """
+    scalar_type = get_scalar_type(num_bits, zeros is not None)
+    return torch.ops._marlin_kernels.machete_gemm(
+        A, B, scalar_type, scales, zeros, group_size, C, alpha, beta, schedule
+    )
+
+
+def machete_prepack_B(B: torch.Tensor, num_bits: int, has_zp: bool) -> torch.Tensor:
+    """Repack quantized weights for Machete kernels."""
+    scalar_type = get_scalar_type(num_bits, has_zp)
+    return torch.ops._marlin_kernels.machete_prepack_B(B, scalar_type)
 
 
 def gptq_marlin_gemm(
@@ -43,7 +78,7 @@ def gptq_marlin_gemm(
     `marlin_gemm` that supports converted GPTQ kernels.
     """
     scalar_type = get_scalar_type(num_bits, has_zp)
-    return torch.ops._marlin_kernels_ops.gptq_marlin_gemm(
+    return torch.ops._marlin_kernels.gptq_marlin_gemm(
         a,
         b_q_weight,
         b_scales,
@@ -77,7 +112,7 @@ def gptq_marlin_24_gemm(
     `marlin_gemm` that supports 2:4 sparsity.
     """
     scalar_type = get_scalar_type(num_bits, False)
-    return torch.ops._marlin_kernels_ops.gptq_marlin_24_gemm(
+    return torch.ops._marlin_kernels.gptq_marlin_24_gemm(
         a, b_q_weight, b_meta, b_scales, workspace, scalar_type, size_m, size_n, size_k
     )
 
@@ -90,7 +125,7 @@ def gptq_marlin_repack(
     num_bits: int,
 ) -> torch.Tensor:
     """Repack GPTQ parameters for Marlin kernels."""
-    return torch.ops._marlin_kernels_ops.gptq_marlin_repack(
+    return torch.ops._marlin_kernels.gptq_marlin_repack(
         b_q_weight, perm, size_k, size_n, num_bits
     )
 
@@ -107,7 +142,7 @@ def marlin_gemm(
     """
     Matrix multiplication using Marlin kernels.
     """
-    return torch.ops._marlin_kernels_ops.marlin_gemm(
+    return torch.ops._marlin_kernels.marlin_gemm(
         a, b_q_weight, b_scales, workspace, size_m, size_n, size_k
     )
 
@@ -123,6 +158,21 @@ def fp8_marlin_gemm(
     size_n: int,
     size_k: int,
 ) -> torch.Tensor:
-    return torch.ops._marlin_kernels_ops.fp8_marlin_gemm(
+    return torch.ops._marlin_kernels.fp8_marlin_gemm(
         a, b_q_weight, b_scales, workspace, num_bits, size_m, size_n, size_k
     )
+
+
+__all__ = [
+    "ScalarType",
+    "scalar_types",
+    "awq_marlin_repack",
+    "machete_supported_schedules",
+    "machete_gemm",
+    "machete_prepack_B",
+    "gptq_marlin_gemm",
+    "gptq_marlin_24_gemm",
+    "gptq_marlin_repack",
+    "marlin_gemm",
+    "fp8_marlin_gemm",
+]
