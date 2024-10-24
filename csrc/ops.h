14613bf27b4eee2ee305f58ccfb403c25490e626
Daniël de Kok <me@danieldk.eu>
2024-10-24 09:28:21 +0200

    Add w8a8 scaled matmul kernels from vLLM (#6)

--- csrc/ops.h
+++ csrc/ops.h
@@ -2,6 +2,21 @@
 
 #include <torch/library.h>
 
+bool cutlass_scaled_mm_supports_fp8(int64_t cuda_device_capability);
+
+void cutlass_scaled_mm(torch::Tensor& out, torch::Tensor const& a,
+                       torch::Tensor const& b, torch::Tensor const& a_scales,
+                       torch::Tensor const& b_scales,
+                       c10::optional<torch::Tensor> const& bias);
+
+void cutlass_scaled_mm_azp(torch::Tensor& out, torch::Tensor const& a,
+                           torch::Tensor const& b,
+                           torch::Tensor const& a_scales,
+                           torch::Tensor const& b_scales,
+                           torch::Tensor const& azp_adj,
+                           c10::optional<torch::Tensor> const& azp,
+                           c10::optional<torch::Tensor> const& bias);
+
 void static_scaled_fp8_quant(torch::Tensor& out, torch::Tensor const& input,
                              torch::Tensor const& scale);
 
